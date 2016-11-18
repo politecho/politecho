8d0be940d320a7a785cc105f7c65f204a3fec45d
Jonathan Zong <jonathan@jonathanzong.com>
2016-11-18 10:25:27 -0800

    use names

--- id_finder/js/chart.js
+++ id_finder/js/chart.js
@@ -158,7 +158,7 @@ function loadChart(userData) {
             return false;
         })
         .on("mouseover", function(d){
-            tooltip.text(d.userId);
+            tooltip.text(d.name);
             return tooltip.style("visibility", "visible");
         })
         .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
