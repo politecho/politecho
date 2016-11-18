d9f017be29ce089d300aa9fedfb172344d84d7fd
Zachary Liu <liu.zachary@gmail.com>
2016-11-18 00:41:52 -0800

    Parse friends list

--- id_finder/parse.js
+++ id_finder/parse.js
@@ -8,7 +8,7 @@ function parseDOM(document_root) {
 	return ids.toString();
 }
 
-function getFriends() {
+function getFriends(done) {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'https://www.facebook.com', true);
 	xhr.onreadystatechange = function (e) {
@@ -21,28 +21,37 @@ function getFriends() {
 			var uniqueIds = ids.filter(function (item, pos) {
 				return ids.indexOf(item) == pos;
 			})
-			console.log(uniqueIds);
+			done(uniqueIds);
 		}
 	}
 	xhr.send();
 }
 
 function parsePage(url, done) {
-	console.log(url);
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', url, true);
 	xhr.onreadystatechange = function (e) {
 		if (xhr.readyState == 4) {
 			var text = xhr.responseText;
 			var $t = $(text);
-			var $q = $($t.find('code').toArray().map(function (e) {
+			var result = $t.find('code').toArray().map(function (e) {
 				return e.innerHTML
 			}).filter(function (e) {
 				return e.indexOf('id="BrowseResultsContainer"') != -1
-			})[0].slice(5, -4));
-			var ids = $q.find('[data-hovercard]').map(function (e) {
-				return /id=([\d]+)/.exec($(this).attr('data-hovercard'))[1]
-			}).get();
+			})[0];
+
+			if (!result) {
+				// console.log('empty result:', url);
+				done([]);
+			} else {
+				var $q = $(result.slice(5, -4));
+				var ids = $q.find('[data-hovercard]').map(function (e) {
+					return /id=([\d]+)/.exec($(this).attr('data-hovercard'))[1]
+				}).get();
+				console.log('results:', ids);
+				done(ids);
+			}
+
 			// debugger;
 			// $(text).find('strong a').each(function () {
 			// 	console.log($(this).attr('href'));
@@ -71,12 +80,12 @@ function parsePage(url, done) {
 			// 	var end = start + text.substring(start).indexOf("}") + 1;
 			// 	ids.push(JSON.parse(text.slice(start,end)).share_id);
 			// }
-			if (ids.length > 0) {
-				chrome.runtime.sendMessage({
-					action: "parseResponse",
-					source: ids
-				});
-			}
+			// if (ids.length > 0) {
+			// 	chrome.runtime.sendMessage({
+			// 		action: "parseResponse",
+			// 		source: ids
+			// 	});
+			// }
 		}
 	}
 	xhr.send();
@@ -94,11 +103,60 @@ function buildQueryUrl(userId, newsSourceIds) {
 	return url;
 }
 
+function getAllPageIds() {
+	return Object.keys(news_dict)
+		.concat(Object.keys(fakenews_dict))
+		.concat(Object.keys(pol_dict));
+}
+
+function getUserScore(userId, done) {
+	var pageIds = getAllPageIds();
+	var urls = [];
+	while (pageIds.length > 0) {
+		var url = buildQueryUrl(userId, pageIds.splice(0, 5));
+		urls.push(url);
+	}
+	var returnedCount = 0;
+	var foundPageIds = [];
+	urls.forEach(function (url) {
+		parsePage(url, function (thisIds) {
+			foundPageIds.push.apply(foundPageIds, thisIds);
+			returnedCount++;
+			if (returnedCount == urls.length) {
+				done(score(foundPageIds));
+			}
+		});
+	});
+}
+
+function getAllFriendScores(done) {
+	getFriends(function (userIds) {
+		// TODO: remove limit
+		userIds = userIds.splice(0, 10);
+
+		var results = [];
+		userIds.forEach(function (userId) {
+			getUserScore(userId, function (score) {
+				results.push({
+					userId: userId,
+					score: score,
+				});
+				if (results.length == userIds.length) {
+					done(results);
+				}
+			});
+		});
+	});
+}
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	console.log("Incoming message", request, sender);
 	if (request.action == "parse") {
 		// parsePage(buildQueryUrl(request.userId, request.newsSourceIds));
-		getFriends();
+		// getFriends();
+		getAllFriendScores(function (results) {
+			console.log(results);
+		});
 		sendResponse('a');
 	}
 });
